5aa3f35b420cb704c853f3993aa5fe1f486b55d7
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-19 19:20:12 +0500

    jkh

--- script.js
+++ script.js
@@ -66,6 +66,8 @@ console.log(timezone_offset_in_seconds(dt));
 dt = new Date(1989, 10, 1); 
 console.log(timezone_offset_in_seconds(dt));
 
+
+
 // Write a JavaScript function to add specified years to a date.   
 // Test Data :
 // dt = new Date(2014,10,2);
