db1d987639609b76c0bad5d3011d58d94ec8688c
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-19 20:21:20 +0500

    cms

--- script.js
+++ script.js
@@ -330,9 +330,6 @@ dt = new Date();
 console.log(startOfMonth(dt).toString());
 
 
-
-
-
 // Write a JavaScript function to get the month end date.  
 
 function endOfMonth(date)
@@ -345,3 +342,4 @@ function endOfMonth(date)
 dt = new Date(); 
 
 console.log(endOfMonth(dt).toString());
+
