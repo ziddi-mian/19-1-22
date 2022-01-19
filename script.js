a3768a6a637ae4b8c62919643dc2af71f84dad17
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-19 20:18:24 +0500

    ms

--- script.js
+++ script.js
@@ -135,14 +135,6 @@ console.log(add_months(dt, 10).toString());
 // console.log(diff_minutes(dt1, dt2));
 // 2
 
-
-
-
-
-
-
-
-
 function diff_minutes(dt2, dt1) 
  {
 
@@ -179,6 +171,7 @@ console.log(diff_hours(dt1, dt2));
 dt1 = new Date("October 13, 2014 08:11:00");
 dt2 = new Date("October 13, 2014 11:13:00");
 console.log(diff_hours(dt1, dt2));
+
 
 
 
